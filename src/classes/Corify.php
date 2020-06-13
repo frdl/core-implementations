328b6d0ced84eceaa8150836f0978267c5e22c0b
Till Wehowski <github@webfan.de>
2020-06-13 07:44:36 +0200

    Update Corify.php

--- src/classes/Corify.php
+++ src/classes/Corify.php
@@ -6,11 +6,20 @@ namepspace Frdlweb;
 class Corify
 {
 
+   const ALIAS_MAP = [
+      \frdl\ProjectState::class => \Webfan\Support\Project\State::class,
+      
+   ];
+   
+   
+   
    public function __construct(){
              $this->loadAliasClasses();
    }
 
    public function loadAliasClasses(){
-   
+       foreach(static::ALIAS_MAP as $alias => $realClass){
+          \class_alias($realClass, $alias);
+       }
    }
 }
