0d8eef320532e43be24709fda9b6e7a319318179
Till Wehowski <github@webfan.de>
2020-06-13 08:20:55 +0200

    Create State.php

---
+++ src/fascades/State.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Project;
+
+use frdl\core\Facade;
+
+
+class State extends Facade
+{
+
+  
+
+    protected static function getFacadeAccessor() :string
+    {
+         return \frdl\ProjectState::class;
+    }
+    
+    
+    public static function getFacadeInstance()
+    {
+        $class = static::getFacadeAccessor();
+    
+         $i = new $class();
+         
+         return $i;
+    }
+    
+    
+
+   
+}
